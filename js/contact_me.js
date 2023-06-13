26ab4ccd33dedb4dbacbc0855e1596bd18b9644f
Richard Kingston <richard.kingston@manchester.ac.uk>
2023-06-13 21:41:38 +0100

    Add files via upload

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,7 +23,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "./bin/contact_me.php",
+                url: "../contact.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -50,7 +50,8 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems the mail server is not responding...</strong><br>");
+                    $('#success > .alert-danger').append("You can email us directly at <a href=\"mailto:honleycc@gmail.com?Subject=E-mail from HCC web form\">honleycc@gmail.com</a>. Sorry for the inconvenience!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
